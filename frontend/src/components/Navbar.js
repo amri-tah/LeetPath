b88917d026dd57300f28f51e3a3aad78c8fa4b99
amri-tah <amrithanandini2003@gmail.com>
2024-10-22 21:40:32 +0530

    fixed link

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div className='flex px-10 py-5 text-xl justify-between mx-10 my-5 text-white bg-black rounded-xl top-0 sticky'>
       <h1 className='font-lexend '>{"{LeetPath}"}</h1>
       <div className='flex gap-10 font-montserrat'>
-        <h1><a href='/'>Home</a></h1>
-        <h1><a href='/login'>Login</a></h1>
+        <h1><Link href='/'>Home</Link></h1>
+        <h1><Link href='/login'>Login</Link></h1>
       </div>
     </div>
   )
