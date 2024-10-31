ed4aca85fd57b7a4944c78eb6d9fdf70448e18f8
amri-tah <amrithanandini2003@gmail.com>
2024-10-31 20:57:15 +0530

    added code editor page

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "LeetPath",
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
       <body className='font-montserrat'>
         <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
