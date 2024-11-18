f9e7490f3692a4c4c5f6a43c3b5c6f2dc34ffde5
Vishal <117697246+VishalTheHuman@users.noreply.github.com>
2024-11-18 22:06:31 +0530

    API

--- backend/model/test.py
+++ backend/model/test.py
@@ -1,8 +1,8 @@
-# #
-# #
-# #      Upload File
-# #
-# #
+#
+#
+#      Upload File
+#
+#
 
 import requests
 
@@ -20,65 +20,65 @@ print(response.json())
 
 
 
-# #
-# #
-# #      Rename File
-# #
-# #
+#
+#
+#      Rename File
+#
+#
 
-# import requests
+import requests
 
-# url = 'http://127.0.0.1:5000/rename'
-# data = {
-#     'old_name': 'data.png',
-#     'new_name': 'image.png'
-# }
+url = 'http://127.0.0.1:5000/rename'
+data = {
+    'old_name': 'data.png',
+    'new_name': 'image.png'
+}
 
-# response = requests.post(url, json=data)
-# print(response.json())
-
-
-# #
-# #
-# #      Download File
-# #
-# #
-
-# import requests
-# import os
-
-# url = 'http://127.0.0.1:5000/download'
-# data = {'filename': 'image.png'}
-
-# response = requests.post(url, json=data)
-
-# if response.status_code == 200:
-#     content_disposition = response.headers.get('Content-Disposition')
-#     if content_disposition:
-#         filename = content_disposition.split('filename=')[-1].strip('"')
-#     else:
-#         filename = data['filename']
-
-#     filename = os.path.basename(filename)
-
-#     with open(filename, 'wb') as f:
-#         f.write(response.content)
-#     print(f'File "{filename}" downloaded successfully.')
-# else:
-#     print(f"Error: {response.json().get('error')}")
+response = requests.post(url, json=data)
+print(response.json())
 
 
-# #
-# #
-# #      Delete File
-# #
-# #
+#
+#
+#      Download File
+#
+#
+
+import requests
+import os
+
+url = 'http://127.0.0.1:5000/download'
+data = {'filename': 'image.png'}
+
+response = requests.post(url, json=data)
+
+if response.status_code == 200:
+    content_disposition = response.headers.get('Content-Disposition')
+    if content_disposition:
+        filename = content_disposition.split('filename=')[-1].strip('"')
+    else:
+        filename = data['filename']
+
+    filename = os.path.basename(filename)
+
+    with open(filename, 'wb') as f:
+        f.write(response.content)
+    print(f'File "{filename}" downloaded successfully.')
+else:
+    print(f"Error: {response.json().get('error')}")
 
 
-# import requests
+#
+#
+#      Delete File
+#
+#
 
-# url = 'http://127.0.0.1:5000/delete'
-# params = {'filename': 'image.png'}
 
-# response = requests.delete(url, params=params)
-# print(response.json())
+import requests
+
+url = 'http://127.0.0.1:5000/delete'
+params = {'filename': 'image.png'}
+
+response = requests.delete(url, params=params)
+print(response.json())
